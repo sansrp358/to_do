4b46a1f341ae994ecd3952c102fe335d812df5dd
sansrp358 <sanskrutirpaitwar@gmail.com>
2020-08-28 01:16:01 +0530

    Updates

---
+++ precache-manifest.dcf0f5b027ee0feb40d865a9e7041a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72083dab13a4d955991d8c071af1a5c6",
+    "url": "/to_do/index.html"
+  },
+  {
+    "revision": "f394316571c0988f583a",
+    "url": "/to_do/static/css/main.0bf4ef08.chunk.css"
+  },
+  {
+    "revision": "166dd2615f26ae42f887",
+    "url": "/to_do/static/js/2.96c40f12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_do/static/js/2.96c40f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f394316571c0988f583a",
+    "url": "/to_do/static/js/main.2962ff39.chunk.js"
+  },
+  {
+    "revision": "5b6fe0a39d95288cbfe4",
+    "url": "/to_do/static/js/runtime-main.8663b774.js"
+  }
+]);
